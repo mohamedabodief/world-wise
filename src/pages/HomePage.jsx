cb0b50781f0c484c86f423f9a4444604ef631257
mohamed abodief <mohamed.abodeaf@gmail.com>
2025-12-05 12:33:54 +0200

    add some css module files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
-import PageNav from "../components/PageNav"
+import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
+import AppNav from '../components/AppNav';
 
 function HomePage() {
-    return (
-        <div>
-            <PageNav />
-            <h1>HomePage</h1>
-        </div>
-    )
+  return (
+    <div>
+      <PageNav />
+      <AppNav />
+      <h1>HomePage</h1>
+      <Link to="/AppLayout">Go to App Layout</Link>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
